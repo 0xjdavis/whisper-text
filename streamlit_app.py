c539a9f29f6d56ceb8e74891c6e435de05365e5d
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-29 00:35:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+from openai import OpenAI
+client = OpenAI()
 
-st.title("🎈 My new app")
+audio_file = open("/path/to/file/speech.mp3", "rb")
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file, 
+  response_format="text"
+)
+
+st.title("🎈 Whisper Text")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+print(transcription.text)
