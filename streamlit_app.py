8b6e7f3b676ac31b3e8b3725d9a64f0b63c5e697
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-29 00:46:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 
-openai_api_key = st.secrets["open_ai_key]
+openai_api_key = st.secrets["open_ai_key"]
 client = OpenAI(api_key=openai_api_key)
 
 audio_file = open("/path/to/file/speech.mp3", "rb")
